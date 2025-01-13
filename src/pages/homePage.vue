2f200cb4128433e9bd247a859f09e3fbba8905b0
Petr <vasutik@mail.ru>
2025-01-13 16:38:01 +0300

    init commit

---
+++ src/pages/homePage.vue
@@ -0,0 +1,19 @@
+<template>
+  <p>Основное содержимое страницы</p>
+</template>
+
+<script>
+
+
+export default {
+  components: {  },
+  data() {
+    return {
+      
+    }
+  },
+  methods: {
+    
+  }
+}
+</script>
