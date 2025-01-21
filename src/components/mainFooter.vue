f1d82d28b74a9b188206aeb55c5f7348d80a0a72
Petr <vasutik@mail.ru>
2025-01-21 17:19:49 +0300

    добавляет разметку для планшета

--- src/components/mainFooter.vue
+++ src/components/mainFooter.vue
@@ -1,18 +1,20 @@
 <template>
-  <div class="container">
-      <ul class="navbar-list">
-        <li class="navbar-item" v-for="link in links" :key="link.alias">
-            <router-link class="navbar-link" :to="link.url">{{ link.title }}</router-link>
+  <div class="main-footer">
+    <div class="container">
+      <ul class="navbar__list">
+        <li class="navbar__item" v-for="link in links" :key="link.alias">
+            <router-link class="navbar__link" :to="link.url">{{ link.title }}</router-link>
           </li>
       </ul>
 
       <!-- link to course -->
-      <ul class="navbar-list" style="margin-top: 14px;">
-        <li class="navbar-item" style="opacity: 0.6; font-size: 14px;">
-          <router-link to="https://tocode.ru" class="navbar-link">Сделано на курсе "Vue.js 3"</router-link>
+      <ul class="navbar__list" style="margin-top: 14px;">
+        <li class="navbar__item" style="opacity: 0.6; font-size: 14px;">
+          <router-link to="https://tocode.ru" class="navbar__link">Сделано на курсе "Vue.js 3"</router-link>
         </li>
       </ul>
     </div>
+  </div>
 </template>
 <script>
 import {app, links} from "../_config.js";
@@ -25,3 +27,8 @@ export default {
   },
 };
 </script>
+<style lang="scss">
+.main-footer {
+  background-color: #06255B;
+}
+</style>
--- src/components/mainHeader.vue
+++ src/components/mainHeader.vue
@@ -1,15 +1,46 @@
 <template>
   <header class="navbar">
-    <div class="container">
-      <div class="navbar__content">
-        <router-link class="navbar__logo" to="/">
-          <div navbar__logo-container>
-            <img src="@/img/logo.svg" width="263" height="46" alt="Логотип">
+    <div class="navbar__content">
+      <router-link class="navbar__logo" to="/">
+        <div class="navbar__logo-container">
+          <img src="@/img/logo.svg" width="263" height="46" alt="Логотип">
+        </div>
+      </router-link>
+      <ul class="navbar__list">
+        <li class="navbar__item" v-for="link in links" :key="link.alias">
+          <router-link class="navbar__link" :to="link.url">{{ link.title }}</router-link>
+        </li>
+      </ul>
+      <div class="navbar__btn-list">
+        <div class="navbar__contacts">
+          <span>Contacts Us</span>
+        </div>
+        <div class="navbar__lang">
+          <div class="navbar__icon-container">
+            <button class="navbar__icon">
+              <img src="@/img/germany-icon.svg" width="39" height="18" alt="">
+            </button>
+            <button class="navbar__icon">
+              <img src="@/img/english-icon.svg" width="39" height="18" alt="">
+            </button>
+            <button class="navbar__icon">
+              <img src="@/img/french-icon.svg" width="39" height="18" alt="">
+            </button>
           </div>
-        </router-link>
-        <ul class="navbar-list">
-          <li class="navbar-item" v-for="link in links" :key="link.alias">
-            <router-link class="navbar-link" :to="link.url">{{ link.title }}</router-link>
+        </div>
+        <button class="navbar-toggle">
+          <div class="navbar-toggle__icon navbar-toggle__icon--open">
+            <img src="@/img/toggle-open.svg" width="33" height="26" alt="">
+          </div>
+          <div class="navbar-toggle__icon navbar-toggle__icon--close">
+            <img src="@/img/toggle-close.svg" width="21" height="21" alt="">
+          </div>
+        </button>
+      </div>
+      <div class="navbar__mobile">
+        <ul class="navbar__list">
+          <li class="navbar__item" v-for="link in links" :key="link.alias">
+            <router-link class="navbar__link" :to="link.url">{{ link.title }}</router-link>
           </li>
         </ul>
       </div>
@@ -27,10 +58,3 @@ export default {
   },
 };
 </script>
-<style lang="scss">
-.navbar {
-  position: absolute;
-  top: 0;
-  left: 0;
-}
-</style>
